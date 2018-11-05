7c6a51b369f0b7ad39f9f3b32c065f3797b35fe8
AleksRu <aleksruzhkov@ya.ru>
2018-11-05 13:26:52 +0300

    setting views

--- resources/views/datatable/status.blade.php
+++ resources/views/datatable/status.blade.php
@@ -1,7 +1,7 @@
 @if (!$status && !$status_code)
-    <h3><span class="badge badge-success">New</span></h3>
+    <h3><span class="badge bg-green">New</span></h3>
 @elseif ($status && !$status_code)
-    <h5><span class="badge badge-info">В работе</span></h5>
+    <h5><span class="badge bg-blue">В работе</span></h5>
 @elseif ($status_code)
-    <h5><span class="badge badge-secondary">Завершено</span></h5>
+    <h5><span class="badge">Завершено</span></h5>
 @endif
