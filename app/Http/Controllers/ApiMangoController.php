c768a12d7c2ff2623b691d5b524f26f9c27b389e
AleksRu <aleksruzhkov@ya.ru>
2018-11-06 19:52:03 +0300

    add control outgoing calls

--- app/Http/Controllers/ApiMangoController.php
+++ app/Http/Controllers/ApiMangoController.php
@@ -16,7 +16,8 @@ class ApiMangoController extends Controller
     {
         $data = json_decode($request->json, true);
 
-        //отладочные данные
+//отладочные данные
+        //входищий
 //        $dataRequest = [
 //            'json' => ["seq" => 1,"call_state" => "Appeared","location" => "ivr",
 //              "from" => ["number" => "79264819924"],
@@ -25,32 +26,59 @@ class ApiMangoController extends Controller
 //        ];
 //        $data = $dataRequest['json'];
 
-        //Проверка на первый входящий звонок
-        if($data['seq'] == 1 && $data['location'] == 'ivr' && $data['call_state'] == 'Appeared'){
-            //ищем клиента по номеру телефона
-            $client = Client::getOnPhone($data['from']['number'])->first();
-            //ищем магазин
-            $store = Store::where('phone', $data['to']['number'])->first();
-            //если новый клиент - создаем заявку
-            if ( !$client ){
-                $client = Client::create(['phone' => $data['from']['number']]);
+        //исходящий
+//        $dataRequest = [
+//            'json' => ["seq" => 1,"call_state" => "Appeared","location" => "abonent",
+//              "from" => ["extension" => "111"],
+//              "to" => ["number" => "79264819924"]
+//              ],
+//        ];
+//        $data = $dataRequest['json'];
 
-                Order::create([
-                    'name_customer' => 'Входящий Звонок',
+        //Проверка на первый звонок
+        if($data['seq'] === 1 && $data['call_state'] === 'Appeared'){
+            //проверка на входящий звонок
+            if ($data['location'] === 'ivr') {
+                //ищем клиента по номеру телефона
+                $client = Client::getOnPhone($data['from']['number'])->first();
+                //ищем магазин
+                $store = Store::where('phone', $data['to']['number'])->first();
+                //если новый клиент - создаем заявку
+                if ( !$client ){
+                    $client = Client::create(['phone' => $data['from']['number']]);
+
+                    Order::create([
+                        'name_customer' => 'Входящий Звонок',
+                        'client_id' => $client->id,
+                        'store' => $store->name ?? 'Не определен',
+                        'phone' => $data['from']['number'],
+                        'total' =>'0',
+                        'comment' =>'-',
+                        'products' => Product::EMPTY_PRODUCTS
+                    ]);
+                }
+                //фиксируем звонок
+                ClientCall::create([
                     'client_id' => $client->id,
-                    'store' => $store->name ?? 'Не определен',
-                    'phone' => $data['from']['number'],
-                    'total' =>'0',
-                    'comment' =>'-',
-                    'products' => Product::EMPTY_PRODUCTS
+                    'type' => ClientCall::incomingCall,
+                    'store_id' => $store->id ?? NULL
                 ]);
             }
-            //фиксируем звонок
-            ClientCall::create([
-                'client_id' => $client->id,
-                'type' => ClientCall::incomingCall,
-                'store_id' => $store->id ?? NULL
-            ]);
+
+            //проверка на исходящий вызов
+            if ($data['location'] ==='abonent' && array_key_exists('extension', $data['from'])){
+                //ищем клиента по номеру телефона
+                $client = Client::getOnPhone($data['to']['number'])->first();
+                if ($client) {
+                    //фиксируем звонок
+                    ClientCall::create([
+                        'client_id' => $client->id,
+                        'type' => ClientCall::outgoingCall,
+                    ]);
+                }
+
+            }
+
         }
 
         return ['status' => 200];
