0c487a8939833635d8296dd8f22d6b2659b336df
aleksru <aleksru@ya.ru>
2018-11-10 12:53:13 +0300

    add migrations

---
+++ app/Privilege.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Privilege extends Model
+{
+    protected $table = 'privilege';
+    public $timestamps = false;
+}
---
+++ database/migrations/2018_11_10_121231_create_privilege_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrivilegeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('privilege', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('privilege');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('privilege');
+    }
+}
