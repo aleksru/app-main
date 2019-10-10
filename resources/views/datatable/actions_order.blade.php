96ea9fbab2d4cd35820c90589ce2777036698633
aleksru <aleksruzhkov@yandex.ru>
2019-10-10 20:25:51 +0300

    Add views users to main table

--- resources/views/datatable/actions_order.blade.php
+++ resources/views/datatable/actions_order.blade.php
@@ -3,7 +3,7 @@
     <i class="fa fa fa-sign-in btn btn-xs btn-success"></i>
 </a>
 @if($order->views->count() > 0)
-    <i class="fa fa-eye" aria-hidden="true"></i>
+    <i class="fa fa-eye" aria-hidden="true" title="{{$order->views->implode('name', ', ')}}"></i>
 @endif
 
 {{--<button class="btn btn-xs btn-warning btn-status" data-route="{{ route('orders.set-status', $order->id) }}"--}}
