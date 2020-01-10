7e407aec9891f536cdda84031a4f94f5116ffa6c
aleksru <aleksruzhkov@yandex.ru>
2020-01-10 18:40:52 +0300

    #Fix jivo

--- app/Http/Controllers/Api/JivoChatController.php
+++ app/Http/Controllers/Api/JivoChatController.php
@@ -4,10 +4,11 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
-class JivoChatController
+class JivoChatController extends Controller
 {
     public function webhooks(Request $request)
     {
