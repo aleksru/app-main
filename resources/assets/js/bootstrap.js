77407e47955b3e514b1384ac7e9e2d63cdcd1da9
aleksru <aleksruzhkov@yandex.ru>
2020-02-13 20:43:37 +0300

    Add ajax redirect 401

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -13,6 +13,11 @@ try {
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        statusCode: {
+            401: function(){
+                window.location.href = '/';
+            }
         }
     });
     require('bootstrap-sass');
