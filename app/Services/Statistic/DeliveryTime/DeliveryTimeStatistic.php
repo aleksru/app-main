c88b9782ae715d532f77b558d184aca189ee40d4
aleksru <aleksruzhkov@yandex.ru>
2020-05-19 13:48:53 +0300

    refactor delivery statistic

--- app/Services/Statistic/DeliveryTime/DeliveryTimeStatistic.php
+++ app/Services/Statistic/DeliveryTime/DeliveryTimeStatistic.php
@@ -9,11 +9,9 @@ use App\Services\Statistic\Abstractions\IGeneralStatisticGenerate;
 use App\Services\Statistic\GeneralStatistic\GeneralStatistic;
 use Carbon\Carbon;
 
-class DeliveryTimeStatistic extends BaseStatistic implements IGeneralStatisticGenerate
+class DeliveryTimeStatistic extends BaseStatistic
 {
     protected $repository;
-    protected $avgSales;
-    protected $sumSales;
 
     /**
      * DeliveryTimeStatistic constructor.
@@ -23,12 +21,13 @@ class DeliveryTimeStatistic extends BaseStatistic implements IGeneralStatisticGe
     public function __construct(Carbon $dateFrom, Carbon $dateTo)
     {
         parent::__construct( $dateFrom,  $dateTo);
-        $this->repository = new DeliveryTimeStatisticRepository();
+        $this->repository = new DeliveryTimeStatisticRepository($dateFrom, $dateTo);
     }
     public static function createTableRender(string $routeIndex, string $routeDatatable, string $labelHeader = null, string $name = null) : BaseReportTableRender
     {
         return new DeliveryTimeTableRender( $routeIndex,  $routeDatatable,  $labelHeader,  $name);
     }
+
     public static function createDefaultTableRender() : BaseReportTableRender
     {
         return new DeliveryTimeTableRender(
@@ -44,107 +43,16 @@ class DeliveryTimeStatistic extends BaseStatistic implements IGeneralStatisticGe
 
     public function generateGenerals()
     {
-        (new GeneralStatistic($this->container, $this))->genAll();
+        (new GeneralStatistic($this->container, $this->repository, $this))->genAll();
     }
 
-    public function genDone()
+    static function createDataItem($field) : DeliveryTimeStatisticItem
     {
-        $result = [];
-        $items = $this->repository->getDoneCount($this->dateFrom, $this->dateTo);
-        foreach ($items as $item){
-            $field = new DeliveryTimeStatisticItem($item->delivery_desc);
-            $field->setDone($item->cnt);
-            $result[] = $field;
-        }
-
-        return $result;
+        return new DeliveryTimeStatisticItem($field);
     }
 
-    public function genMissed()
+    protected function getOrCreateFieldOnContainer($key) : DeliveryTimeStatisticItem
     {
-        $result = [];
-        $items = $this->repository->getMissedCount($this->dateFrom, $this->dateTo);
-        foreach ($items as $item){
-            $field = new DeliveryTimeStatisticItem($item->delivery_desc);
-            $field->setMissed($item->cnt);
-            $result[] = $field;
-        }
-
-        return $result;
+        return parent::getOrCreateFieldOnContainer($key);
     }
-
-    public function genProfit()
-    {
-        $result = [];
-        $items = $this->repository->getProfitOnDates($this->dateFrom, $this->dateTo);
-        foreach ($items as $item){
-            $field = new DeliveryTimeStatisticItem($item->delivery_desc);
-            $field->setProfit($item->profit);
-            $result[] = $field;
-        }
-
-        return $result;
-    }
-
-    public function genAvgInvoice()
-    {
-        $result = [];
-        $items = $this->getAvgSales();
-        foreach ($items as $item){
-            $field = new DeliveryTimeStatisticItem($item->delivery_desc);
-            $field->setAvgInvoice($item->avg_check);
-            $result[] = $field;
-        }
-
-        return $result;
-    }
-
-    public function genAvgProfit()
-    {
-        $result = [];
-        $items = $this->getAvgSales();
-        foreach ($items as $item){
-            $field = new DeliveryTimeStatisticItem($item->delivery_desc);
-            $field->setAvgProfit($item->avg_profit);
-            $result[] = $field;
-        }
-
-        return $result;
-    }
-
-    public function getSumSales()
-    {
-        if($this->sumSales === null ){
-            $this->sumSales = $this->repository->getCountSalesByDates($this->dateFrom, $this->dateTo);
-        }
-        return $this->sumSales;
-    }
-
-    public function getAvgSales()
-    {
-        if(! $this->avgSales ){
-            $this->genAvgSales();
-        }
-
-        return $this->avgSales;
-    }
-
-    public function genAvgMainInvoice()
-    {
-        $result = [];
-        $items = $this->repository->getAllSalesOnDates($this->dateFrom, $this->dateTo);
-        foreach ($items as $item){
-            $field = new DeliveryTimeStatisticItem($item->delivery_desc);
-            $field->setAvgMainInvoice($item->avg_sales);
-            $result[] = $field;
-        }
-
-        return $result;
-    }
-
-    protected function genAvgSales()
-    {
-        $this->avgSales = $this->repository->getAvgSales($this->dateFrom, $this->dateTo);
-    }
-
 }
--- app/Services/Statistic/DeliveryTime/DeliveryTimeStatisticRepository.php
+++ app/Services/Statistic/DeliveryTime/DeliveryTimeStatisticRepository.php
@@ -4,124 +4,43 @@
 namespace App\Services\Statistic\DeliveryTime;
 
 use App\Enums\ProductCategoryEnums;
+use App\Services\Statistic\Abstractions\GeneralStatisticRepository;
 use App\Services\Statistic\QueryRepository;
 use Carbon\Carbon;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
-class DeliveryTimeStatisticRepository
+class DeliveryTimeStatisticRepository extends GeneralStatisticRepository
 {
-    protected $queryRepository;
 
-    public function __construct()
+    protected function buildJoin(Builder $builder): Builder
     {
-        $this->queryRepository = new QueryRepository();
+        return $builder->join('delivery_periods', 'orders.delivery_period_id', '=', 'delivery_periods.id');
     }
 
-    public function getDoneCount(Carbon $dateFrom, Carbon $dateTo) : Collection
+    protected function getTableName(): string
     {
-//        $db = DB::table('orders')
-//            ->selectRaw('CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc')
-//            ->join('other_statuses', 'orders.stock_status_id', '=', 'other_statuses.id')
-//            ->join('realizations', 'orders.id', '=', 'realizations.order_id')
-//            ->join('products', 'realizations.product_id', '=', 'products.id')
-//            ->join('delivery_periods', 'orders.delivery_period_id', '=', 'delivery_periods.id')
-//            ->whereBetween('orders.created_at', [$dateFrom->toDateString(), $dateTo->toDateString()])
-//            ->whereNull('realizations.deleted_at')
-//            ->where('products.category', '!=', ProductCategoryEnums::DELIVERY)
-//            //->groupBy('delivery_desc')
-//            ->where('other_statuses.result', StatusResults::SUCCESS);
-
-        return $this->queryRepository
-            ->getBaseSuccessRealizationsOnOrdersQuery($dateFrom, $dateTo)
-            ->join('delivery_periods', 'orders.delivery_period_id', '=', 'delivery_periods.id')
-            ->selectRaw('CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc, COUNT(orders.id) AS cnt')
-            ->groupBy('delivery_desc')
-            ->get();
+        return 'delivery_periods';
     }
 
-    public function getMissedCount(Carbon $dateFrom, Carbon $dateTo) : Collection
+    public function getFieldName(): string
     {
-        return $this->queryRepository
-            ->getBaseRefusalRealizationsOnOrdersQuery($dateFrom, $dateTo)
-            ->join('delivery_periods', 'orders.delivery_period_id', '=', 'delivery_periods.id')
-            ->selectRaw('CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc, COUNT(orders.id) AS cnt')
-            ->groupBy('delivery_desc')
-            ->get();
+        return 'delivery_desc';
     }
 
-    public function getProfitOnDates(Carbon $dateFrom, Carbon $dateTo)
+    protected function getGroupByFieldName(): string
     {
-//        SELECT CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc, (SUM(realizations.price) - SUM(realizations.price_opt)) AS profit
-//        from `orders`
-//        inner join `other_statuses` on `orders`.`stock_status_id` = `other_statuses`.`id`
-//        inner join `delivery_periods` on `orders`.`delivery_period_id` = `delivery_periods`.`id`
-//        inner join `realizations` on `orders`.`id` = `realizations`.`order_id`
-//        inner join `products` on `realizations`.`product_id` = `products`.`id`
-//        where `orders`.`created_at` between '2020-04-01' and '2020-05-11' and `products`.`category` != 'DELIVERY' and
-//        `realizations`.`deleted_at` is null and `realizations`.`reason_refusal_id` is NULL  AND other_statuses.result = 1
-//        group by `delivery_desc`
-
-        return $this->queryRepository
-            ->getBaseOrderSalesProfitQuery($dateFrom, $dateTo)
-            ->selectRaw('CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc')
-            ->join('delivery_periods', 'orders.delivery_period_id', '=', 'delivery_periods.id')
-            ->groupBy('delivery_desc')
-            ->get();
+        return 'delivery_desc';
     }
 
-    public function getAvgSales(Carbon $dateFrom, Carbon $dateTo)
+    protected function getSelection(): string
     {
-//            SELECT delivery_desc, AVG(sum_order) AS avg_check, AVG(profit) AS avg_profit FROM
-//            (SELECT CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc, orders.id, SUM(realizations.price) AS sum_order, SUM(realizations.price) - SUM(realizations.price_opt) AS profit
-//                    from `orders`
-//                    inner join `other_statuses` on `orders`.`stock_status_id` = `other_statuses`.`id`
-//                    inner join `delivery_periods` on `orders`.`delivery_period_id` = `delivery_periods`.`id`
-//                    inner join `realizations` on `orders`.`id` = `realizations`.`order_id`
-//                    inner join `products` on `realizations`.`product_id` = `products`.`id`
-//                    where `orders`.`created_at` between '2020-04-01' and '2020-05-11' and `products`.`category` != 'DELIVERY' and
-//                      `realizations`.`deleted_at` is null and `realizations`.`reason_refusal_id` is NULL AND  other_statuses.result = 1
-//                    group by `delivery_desc`, orders.id) AS avd_check
-//            GROUP BY delivery_desc
-        $query = $this->queryRepository
-            ->getBaseOrderSalesProfitQuery($dateFrom, $dateTo)
-            ->selectRaw('CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc, orders.id, SUM(realizations.price) AS sum_order')
-            ->join('delivery_periods', 'orders.delivery_period_id', '=', 'delivery_periods.id')
-            ->groupBy('delivery_desc', 'orders.id');
-        return DB::table(DB::raw("({$query->toSql()}) as sales"))
-            ->mergeBindings($query)
-            ->selectRaw('delivery_desc, AVG(sum_order) AS avg_check, AVG(profit) AS avg_profit')
-            ->groupBy('delivery_desc')
-            ->get();
+        return 'CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc';
     }
 
-    public function getSumSalesByDates(Carbon $dateFrom, Carbon $dateTo)
+    protected function getGroupBy(): string
     {
-        return $this->queryRepository
-                ->getBaseOrderSalesQuery($dateFrom, $dateTo)
-                ->sum('realizations.price');
-    }
-
-
-    public function getCountSalesByDates(Carbon $dateFrom, Carbon $dateTo)
-    {
-        return $this->queryRepository
-            ->getBaseSuccessRealizationsOnOrdersQuery($dateFrom, $dateTo)
-            ->count();
-    }
-
-    public function getAllSalesOnDates(Carbon $dateFrom, Carbon $dateTo)
-    {
-        $query = $this->queryRepository
-            ->getBaseOrderAllSalesQuery($dateFrom, $dateTo)
-            ->selectRaw('CONCAT(delivery_periods.timeFrom, "-", delivery_periods.timeTo) as delivery_desc, SUM(realizations.price) AS sum_order')
-            ->join('delivery_periods', 'orders.delivery_period_id', '=', 'delivery_periods.id')
-            ->groupBy('delivery_desc', 'orders.id');
-
-        return DB::table(DB::raw("({$query->toSql()}) as sales"))
-            ->mergeBindings($query)
-            ->selectRaw('delivery_desc, AVG(sum_order) AS avg_sales')
-            ->groupBy('delivery_desc')
-            ->get();
+        return 'delivery_desc';
     }
 }
