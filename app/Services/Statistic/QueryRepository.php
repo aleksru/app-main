cb9e69e52aac567c704f5157f9347de3bfec9570
aleksru <aleksruzhkov@yandex.ru>
2020-05-21 17:09:44 +0300

    #Fix query category

--- app/Services/Statistic/QueryRepository.php
+++ app/Services/Statistic/QueryRepository.php
@@ -12,48 +12,48 @@ use Illuminate\Support\Facades\DB;
 
 class QueryRepository
 {
-    public function getBaseStatisticOnOrdersQuery(Carbon $dateFrom, Carbon $dateTo) : Builder
+    public function getBaseStatisticOnOrdersQuery(Carbon $dateFrom, Carbon $dateTo): Builder
     {
         return DB::table('orders')
             ->join('other_statuses', 'orders.stock_status_id', '=', 'other_statuses.id')
             ->whereBetween('orders.date_delivery', [$dateFrom->toDateString(), $dateTo->toDateString()]);
     }
 
-    public function getBaseSuccessRealizationsOnOrdersQuery(Carbon $dateFrom, Carbon $dateTo)  : Builder
+    public function getBaseSuccessRealizationsOnOrdersQuery(Carbon $dateFrom, Carbon $dateTo): Builder
     {
         return $this->getBaseStatisticOnOrdersQuery($dateFrom, $dateTo)
-                ->where('other_statuses.result', StatusResults::SUCCESS);
+            ->where('other_statuses.result', StatusResults::SUCCESS);
     }
 
-    public function getBaseRefusalRealizationsOnOrdersQuery(Carbon $dateFrom, Carbon $dateTo)  : Builder
+    public function getBaseRefusalRealizationsOnOrdersQuery(Carbon $dateFrom, Carbon $dateTo): Builder
     {
         return $this->getBaseStatisticOnOrdersQuery($dateFrom, $dateTo)
-                ->where('other_statuses.result', StatusResults::REFUSAL);
+            ->where('other_statuses.result', StatusResults::REFUSAL);
     }
 
-    public function getBaseOrderSalesQuery(Carbon $dateFrom, Carbon $dateTo)  : Builder
+    public function getBaseOrderSalesQuery(Carbon $dateFrom, Carbon $dateTo): Builder
     {
-        return $this->getBaseSuccessRealizationsOnOrdersQuery($dateFrom, $dateTo)
-            ->join('realizations', 'orders.id', '=', 'realizations.order_id')
-            ->join('products', 'realizations.product_id', '=', 'products.id')
-            ->where('products.category', '!=', ProductCategoryEnums::DELIVERY)
-            ->whereNull('realizations.deleted_at')
-            ->whereNull('realizations.reason_refusal_id');
+        return $this->buildBaseRealizations($this->getBaseSuccessRealizationsOnOrdersQuery($dateFrom, $dateTo));
     }
 
-    public function getBaseOrderAllSalesQuery(Carbon $dateFrom, Carbon $dateTo)  : Builder
+    public function getBaseOrderAllSalesQuery(Carbon $dateFrom, Carbon $dateTo): Builder
     {
-        return $this->getBaseStatisticOnOrdersQuery($dateFrom, $dateTo)
-            ->join('realizations', 'orders.id', '=', 'realizations.order_id')
-            ->join('products', 'realizations.product_id', '=', 'products.id')
-            ->where('products.category', '!=', ProductCategoryEnums::DELIVERY)
-            ->whereNull('realizations.deleted_at')
-            ->whereNull('realizations.reason_refusal_id');
+        return $this->buildBaseRealizations($this->getBaseStatisticOnOrdersQuery($dateFrom, $dateTo));
     }
 
-    public function getBaseOrderSalesProfitQuery(Carbon $dateFrom, Carbon $dateTo)  : Builder
+    public function getBaseOrderSalesProfitQuery(Carbon $dateFrom, Carbon $dateTo): Builder
     {
         return $this->getBaseOrderSalesQuery($dateFrom, $dateTo)
             ->selectRaw('(SUM(realizations.price) - SUM(realizations.price_opt)) AS profit');
+    }
+
+    protected function buildBaseRealizations(Builder $builder): Builder
+    {
+        return $builder
+                ->join('realizations', 'orders.id', '=', 'realizations.order_id')
+                ->join('products', 'realizations.product_id', '=', 'products.id')
+                ->whereRaw('IFNULL(products.category, "Без категории") != "' . ProductCategoryEnums::DELIVERY . '"')
+                ->whereNull('realizations.deleted_at')
+                ->whereNull('realizations.reason_refusal_id');
     }
 }
