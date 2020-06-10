6df4960b29215083dbda9d7897b9977c83d81e7a
aleksru <aleksruzhkov@yandex.ru>
2020-06-10 19:15:57 +0300

    fix width

--- resources/views/front/logistic/parts/table_select_courier.blade.php
+++ resources/views/front/logistic/parts/table_select_courier.blade.php
@@ -1,4 +1,4 @@
-<select name="courier_id" class="form-control select-courier" data-order-id="{{$order->id}}" style="min-width: 100%">
+<select name="courier_id" class="form-control select-courier" data-order-id="{{$order->id}}">
     <option value="">Не выбран</option>
     @if($order->courier)
         <option value="{{$order->courier->id}}" selected>{{$order->courier->name}}</option>
