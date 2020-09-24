f5e109fc7b424d95c4c13d9332a8fd9998a3c63f
aleksru <aleksruzhkov@yandex.ru>
2020-09-24 21:59:36 +0300

    change view prod

--- resources/views/front/logistic/parts/table_products.blade.php
+++ resources/views/front/logistic/parts/table_products.blade.php
@@ -1,3 +1,9 @@
-@foreach($products as $product)
-    {{ strlen($product) > 50 ? (substr($product, 0, 50) . '...') : $product }}<br/>
-@endforeach
+
+<div title="{{implode("\n", $products)}}">
+    @foreach($products as $product)
+        @if($loop->iteration > 3)
+            @break
+        @endif
+        {{ strlen($product) > 50 ? (substr($product, 0, 50) . '...') : $product }}<br/>
+    @endforeach
+</div>
